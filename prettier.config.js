89ef065afaeba5998082541c9b4e73b2662f6e2b
Eli <88557639+lishaduck@users.noreply.github.com>
2025-01-20 21:50:26 -0600

    chore(deps): bump
    Astro 5, starlight 0.31.
    Add starlight-tailwind and astro-vtbot for nicer styling.
    Once we start adding actual docs, we'll also want expressive-code-twoslash.

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+// @ts-check
+/**
+ * @import { Config } from 'prettier';
+ */
+
+export default /** @type {Config} */ ({
+  plugins: ['prettier-plugin-astro'],
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
   printWidth: 80,
-};
+});
